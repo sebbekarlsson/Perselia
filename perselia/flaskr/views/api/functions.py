c5af5b12b991711a5fcb2909dc5cfc4667aacbaf
= <sebbekarlsson97@gmail.com>
2015-10-01 22:25:55 +0200

    Validation

--- perselia/flaskr/views/api/functions.py
+++ perselia/flaskr/views/api/functions.py
@@ -5,6 +5,13 @@ from api.users import Users
 
 functions = Blueprint('functions', __name__)
 
+valid_classes = [\
+    'Users'
+]
+
 @functions.route('/api/<function>')
 def call(function):
-    return getattr(globals()[function.split('.')[0].title()](), function.split('.')[1])()
+    if function.split('.')[0].title() in valid_classes:
+        return getattr(globals()[function.split('.')[0].title()](), function.split('.')[1])()
+    else:
+        abort(403)
