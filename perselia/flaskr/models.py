d9990658714d2991828860b4b152c5c7688129bb
= <sebbekarlsson97@gmail.com>
2015-10-02 20:08:17 +0200

    Added default column 'created' for all tables

--- perselia/flaskr/models.py
+++ perselia/flaskr/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, func, create_engine
+from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, func, create_engine, TIMESTAMP
 from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -10,7 +10,10 @@ Session.configure(bind=engine)
 
 sess = Session()
 
-class User(Base):
+class Data():
+    created = Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
+
+class User(Base, Data):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     firstname = Column(String)
@@ -20,11 +23,18 @@ class User(Base):
     password = Column(String)
     admin = Column(Integer, default=0)
 
-class Option(Base):
+class Option(Base, Data):
     __tablename__ = 'options'
     id = Column(Integer, primary_key=True)
     key = Column(String, unique=True)
     value = Column(String)
+
+class Post(Base, Data):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    content = Column(String)
+    
 
 def initialize_database():
     Base.metadata.create_all(engine)
