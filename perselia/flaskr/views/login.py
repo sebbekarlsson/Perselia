4d97ba3b90c1e3e06efa007d6d04c9d140b05dae
= <sebbekarlsson97@gmail.com>
2015-10-11 21:56:52 +0200

    Added wtform

--- perselia/flaskr/views/login.py
+++ perselia/flaskr/views/login.py
@@ -1,11 +1,23 @@
 from flask import Blueprint, render_template, abort, request, Response, session
 from jinja2 import TemplateNotFound
 
+from flask.ext.wtf import Form
+from wtforms import BooleanField, TextField, PasswordField
+
 
 login = Blueprint('login', __name__, template_folder='templates')
 
+class LoginForm(Form):
+    email = TextField('Email')
+    password = PasswordField('Password')
 
 @login.route('/login', methods=['GET', 'POST'])
 def _login():
-    return render_template('login.html')
-    
+
+    form = LoginForm(request.form, csrf_enabled=False)
+
+    if form.validate_on_submit():
+        return render_template('sorry.html')
+        
+
+    return render_template('login.html', form=form)
